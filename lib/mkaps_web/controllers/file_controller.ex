e7ff0490dc2e8c6d00fd88878eb16c6a8326a005
Steven Lau <s@macbook-debian>
2025-08-30 22:17:13 +0800

    fix: add cors

--- lib/mkaps_web/controllers/file_controller.ex
+++ lib/mkaps_web/controllers/file_controller.ex
@@ -4,6 +4,7 @@ defmodule MkapsWeb.FileController do
   def file(conn, _params) do
     uploads_path = Application.fetch_env!(:mkaps, MkapsWeb.MkapsLive)[:uploads_path]
     conn
+    |> put_resp_header("access-control-allow-origin", "*")
     |> put_resp_content_type("application/pdf")
     |> send_file(200, Path.join(uploads_path, "file.pdf"))
   end
